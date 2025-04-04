100a2547672cca9b9d8fe684f222dd161efadfb3
ManthanBR <155616252+ManthanBR@users.noreply.github.com>
2025-04-04 21:58:42 +0530

    update

--- src/settings.js
+++ src/settings.js
@@ -10,7 +10,7 @@ export const Settings = {
     constraints: {
       front: {
         video: {
-          facingMode: { exact: "user" },
+          facingMode: { exact: "environment" },
         },
         audio: true,
       },
@@ -22,7 +22,7 @@ export const Settings = {
       },
       desktop: {
         video: {
-          facingMode: "user",
+          facingMode: "environment",
         },
         audio: true,
       },
